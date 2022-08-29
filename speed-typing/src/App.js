bab2210d5695db9425a5aa5ef7e29cc4126ed256
Vasya Boeva <vs99516@gmail.com>
2022-08-29 23:33:54 +0300

    Add fetching data

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,10 +1,18 @@
-
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect( () => {
+  fetch("https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text")
+  .then(response => response.text())
+  .then(response => setData(response))  
+}, [])
   return (
     <div className="App">
-      Hi!
+      {data}
     </div>
   );
 }
