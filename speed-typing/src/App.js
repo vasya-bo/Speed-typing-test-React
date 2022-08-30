045d7067fae2bc37703808c76e562c2e40b16263
Vasya Boeva <vs99516@gmail.com>
2022-08-30 03:08:02 +0300

    creating different spans for texts, handling key Pressed, finishing text and coloring current key

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -2,17 +2,62 @@ import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
-
-  const [data, setData] = useState(null)
+  const [data, setData] = useState('1234567890')
+  const [textRight, setTextRight] = useState('')
+  const [textInProcces, setTextInProcess] = useState('')
+  const [textNotProccessed, setTextNotProccessed] = useState('')
 
   useEffect( () => {
   fetch("https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text")
   .then(response => response.text())
-  .then(response => setData(response))  
-}, [])
+  .then(response => setData(response))
+}, []);
+
+//useEffect(() => {console.log('upload')}, [textInProcces,textNotProccessed])
+
+function start() {
+  let dataArray = data.split('')
+  let firstLetter = dataArray[0];
+  setTextInProcess(firstLetter);
+  dataArray.shift();
+  setTextNotProccessed(dataArray.join(''));
+}
+
+
+
+function handleKeyPress(event) {
+  if(textNotProccessed.length > 0) {
+    if(event.key === textInProcces){
+  let dataArray = textNotProccessed.split('')
+  let firstLetter = dataArray[0];
+  setTextRight(currentValue => currentValue + textInProcces)
+  setTextInProcess(currentValue => currentValue = firstLetter);
+  dataArray.shift();
+  setTextNotProccessed(dataArray.join(''));}
+  else { console.log('wrong letter')}
+
+}
+  else {
+    setTextRight(currentValue => currentValue = '')
+    setTextInProcess(currentValue => currentValue = '')
+    setTextNotProccessed(currentValue => currentValue = 'End!')
+  }
+
+
+}
+
   return (
     <div className="App">
-      {data}
+
+<button id='start' onClick={()=> start()}>Start!</button>
+      <div className='textArea'  >
+      <span id='text-right'>{textRight}</span>
+      <span id='text-in-process'>{textInProcces}</span>
+      <span id='text-not-proccessed'>{textNotProccessed} </span><br></br>
+      <input type='text' onKeyPress={(event) => handleKeyPress(event)}></input>
+      </div>
+
+      
     </div>
   );
 }
