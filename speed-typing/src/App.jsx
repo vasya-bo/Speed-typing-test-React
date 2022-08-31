3b162f5ca234da8000074ed0e6253f05dd364f02
Vasya Boeva <vs99516@gmail.com>
2022-08-31 04:10:59 +0300

    added timers, accuracy view and speed fixed, hide start button

--- speed-typing/src/App.jsx
+++ speed-typing/src/App.jsx
@@ -6,18 +6,30 @@ function App() {
   const [showResult, setShowResult] = useState(false);
   const [keyPressed, setKeyPressed] = useState(null);
   const [color, setColor] = useState('green');
+  const [startButton, setStartButton] = useState(true);
 
   const [text, setText] = useState({
     Right: '', inProcess: '', notDone: '', numberAll: 0, numberRight: 0, numberWrong: 0,
   });
 
+  const [seconds, setSeconds] = useState({ timer: null, seconds: 0 });
+
+  function startTimer() {
+    const timerID = setInterval(() => {
+      setSeconds((prevValue) => ({ ...prevValue, seconds: prevValue.seconds + 1, timer: timerID }));
+    }, 1000);
+  }
+  function stopTimer() {
+    clearInterval(seconds.timer);
+  }
+
   useEffect(() => {
-  // fetch("https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text")
-  // .then(response => response.text())
-  // .then(response => { setData(response)})
-    setData('12345678890');
+    fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text')
+      .then((response) => response.text())
+      .then((response) => { setData(response); });
+    // setData('12345678890');
     console.log('upload1');
-  }, [data]);
+  }, []);
 
   useEffect(() => {
     console.log('addEvent upload');
@@ -58,6 +70,7 @@ function App() {
       console.log('you are done! ');
 
       setShowResult(true);
+      stopTimer();
     }
   }, [keyPressed, text]);
 
@@ -69,12 +82,14 @@ function App() {
     setText((prevValue) => ({
       ...prevValue, numberAll: dataArray.length + 1, inProcess: firstLetter, notDone: dataArray.join(''),
     }));
+    startTimer();
+    setStartButton(false);
   }
 
   return (
     <div className="App">
 
-      <button id="start" type="button" onClick={() => start()}>Start!</button>
+      {startButton && <button id="start" type="button" onClick={() => start()}>Start!</button>}
 
       <span id="text-right">{text.Right}</span>
       <span id="text-in-process" style={{ backgroundColor: color }}>{text.inProcess}</span>
@@ -86,10 +101,21 @@ function App() {
 
       {showResult && <h2>End!</h2>}
 
-      <div id="statistic-Area">
+      <div id="accuracy-Area">
         Точность:
-        {((text.numberRight * 100) / (text.numberWrong + text.numberRight)).toFixed(1)}
+        {
+        text.numberWrong + text.numberRight !== 0
+          ? ((text.numberRight * 100) / (text.numberWrong + text.numberRight)).toFixed(1) : 100
+        }
         %
+
+      </div>
+      <div id="timer-Area">
+        Скорость:
+        {seconds.seconds !== 0
+          ? Math.floor((text.numberRight * 60) / (seconds.seconds)) : 0}
+        {' '}
+        знак/мин
 
       </div>
     </div>
